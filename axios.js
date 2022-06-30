3c99c450acc31e44d794acec597e428bd4102464
juanjoseospina123321 <juan.jose.ospina@hotmail.com>
2022-06-30 01:07:01 -0500

    cambios

--- axios.js
+++ axios.js
@@ -2,12 +2,13 @@
 const button = document.getElementById("button")
 const enviar = document.getElementById("enviar")
 const actu = document.getElementById("actu")
-const botonEliminar=document.getElementById("botonEliminar")
+const botonEliminar = document.getElementById("botonEliminar")
 
-botonEliminar.addEventListener("click", ()=>{
-    const eliminar=document.getElementById("eliminar")
+botonEliminar.addEventListener("click", () => {
+    const eliminar = document.getElementById("eliminar")
     const url = `https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user/${eliminar.value}`
-    axios.delete(url).then(res=>console.log(res)).catch(err=>console.log(err))
+    axios.delete(url).then(res => {console.log(res) 
+        eliminar.value=''}).catch(err => console.log(err))
 })
 
 
@@ -18,34 +19,46 @@ actu.addEventListener("click", () => {
     const idp = document.getElementById("idp").value
     const actualizar = document.getElementById("actualizar").value
 
+   
+
     const url = `https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user/${actualizar}`
     axios.put(url, {
         name: nombrep,
         age: edadp,
         job: trabajop,
         id: idp
-    }).then(res => console.log(res)).catch(error => console.log(error))
+    }).then(res => {
+        nombrep.value=''
+        edadp.value=''
+        trabajop.value=''
+        idp.value=''
+        actualizar.value=''
+        console.log("es aca!!",res)
+
+    }).catch(error => console.log(error))
 
 })
 
 
 button.addEventListener('click', () => {
-    axios({
-        method: "GET",
-        url: "https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user"
+    const url = "https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user";
+    const imagen = document.getElementById("imagen")
+    axios.get(url)
+        .then(res => {
+            const list = document.getElementById("list")
+            list.innerHTML = ''
+            const respuesta = res.data
+            respuesta.map(respu => {
+                const listItem = document.createElement("li")
+                listItem.innerHTML = `
+               <img src="${respu.avatar}" />
+               <p>${respu.name}, ${respu.age}, ${respu.job}, ${respu.id}</p>
+               `
+                list.appendChild(listItem)
+            })
 
-    }).then(res => {
 
-        const list = document.getElementById("list")
-list.innerHTML=''
-        for (const userInfo of res.data) {
-
-            const listItem = document.createElement("LI")
-            listItem.textContent = `${userInfo.name} , ${userInfo.age}, ${userInfo.job}, ${userInfo.id}`
-            list.appendChild(listItem)
-        }
-        
-    })
+        })
         .catch(error => {
             console.log(error)
         })
@@ -59,7 +72,25 @@ enviar.addEventListener("click", () => {
     const job = document.getElementById("job")
     const id = document.getElementById("id")
 
-    axios({
+
+    const url = 'https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user'
+
+    axios.post(url, {
+        name: nombre.value,
+        age: edad.value,
+        job: job.value,
+        id: id.value,
+    })
+        .then(res => {
+            nombre.value = '',
+                edad.value = '',
+                job.value = '',
+                id.value = '',
+                console.log(res)
+        })
+        .catch(error => console.log(error))
+
+    /* axios({
         method: "POST",
         url: 'https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user',
         data: {
@@ -68,8 +99,124 @@ enviar.addEventListener("click", () => {
             job: job.value,
             id: id.value,
         }
-    }).then(res => console.log(res))
-        .catch(error => console.log(error))
+    }) */
+
+
 })
 
+// con asincronismo y promesas
+async function llamar() {
+    const url = 'https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user'
 
+    const llamado = await axios.get(url)
+        .then(res => {
+
+            const list = document.getElementById("list")
+            list.innerHTML = ''
+            for (const userInfoss of res.data) {
+
+                const elementli = document.createElement("li")
+                elementli.innerHTML = `
+                <p>${userInfoss.name}, ${userInfoss.age}, ${userInfoss.job}, ${userInfoss.id} </p>
+                `
+                list.appendChild(elementli)
+            }
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+// sin asincronismo ni promesas
+function llamar2() {
+    const url = "https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user";
+
+    const extraer = axios.get(url)
+        .then(res => {
+
+            const list = document.getElementById("list")
+            list.innerHTML = ''
+            for (const userInfo of res.data) {
+
+                const listItem = document.createElement("LI")
+                listItem.textContent = `${userInfo.name} , ${userInfo.age}, ${userInfo.job}, ${userInfo.id}`
+                list.appendChild(listItem)
+            }
+
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+/* ENSAYO FETCH------------------------------------------------------------------------------------------------ */
+/* const url = "https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user";
+data = {
+    name: 'FETCH PRUEBA',
+    age: 99,
+    job: 'ENSAYO FETC',
+    id: 900,
+}
+
+async function conFetch(url, data) {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+} */
+
+const mandar = conFetch(url, data).then(response => console.log("OK", response)).catch(error => console.log("NO", error))
+
+/* PUT con fetch */
+/* 
+const url1 = "https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user/1";
+data1 = {
+    name: 'FETCH PRUEBA PUT----',
+    age: 992,
+    job: 'ENSAYO FETC.....',
+    id: 900,
+}
+
+async function fetchPut(url, data) {
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+} */
+const mandar1 = fetchPut(url1, data1).then(response => console.log("OK", response)).catch(error => console.log("NO", error))
+
+
+
+// con asincronismo y promesas------------------------y tambien mas sencillo--------------------------------
+async function llamarsinnada() {
+    const url = 'https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user'
+
+    const llamado = await axios.get(url)
+    const response = await llamado
+    console.log(response.data)
+
+}
+
+async function mandarFacilPUT() {
+    const url = 'https://62b8864c03c36cb9b7c878ad.mockapi.io/api/ejemplo/user'
+
+    const data3 = {
+        name: 'JUAN JOSE OSPINA',
+        age: 00000,
+        job: 'POST MAS FACIL',
+        id: 900,
+    }
+
+    const llamado = await axios.post(url, data3)
+    const response = await llamado
+    console.log(response)
+
+}
